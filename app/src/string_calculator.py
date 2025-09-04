762e4be2ae24b1dabe85aa5632754445ebe53c70
shravan kalyankar <shravankalyankar10@gmail.com>
2025-09-04 17:27:27 +0530

    Refactoring: using inbuilt functions to calculate addition

--- app/src/string_calculator.py
+++ app/src/string_calculator.py
@@ -3,10 +3,7 @@ class StringCalculator:
         if not numbers:
             return 0
         
-        result = 0
         numbers_list = numbers.split(',')
         numbers_list = list(map(lambda x: int(x), numbers_list))
-        for num in numbers_list:
-            result += num
-
+        result = sum(numbers_list)
         return result
