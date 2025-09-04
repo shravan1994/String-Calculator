45ca1c6950dec5e9560209892436423452e117b2
shravan kalyankar <shravankalyankar10@gmail.com>
2025-09-04 17:46:09 +0530

    Refactoring: seperating logic of parsing different delimiters

--- app/src/string_calculator.py
+++ app/src/string_calculator.py
@@ -1,9 +1,20 @@
 class StringCalculator:
+    SUPPORTED_DELIMITERS = ['\n']
+
     def add(self, numbers: str) -> int:
         if not numbers:
             return 0
         
-        numbers_list = numbers.replace('\n', ',').split(',')
+        numbers = self._normalize_delimiters(numbers)
+        
+        numbers_list = numbers.split(',')
         numbers_list = list(map(lambda x: int(x), numbers_list))
+
         result = sum(numbers_list)
         return result
+    
+    def _normalize_delimiters(self, numbers: str) -> str:
+        for delimiter in self.SUPPORTED_DELIMITERS:
+            numbers = numbers.replace(delimiter, ',')
+
+        return numbers
