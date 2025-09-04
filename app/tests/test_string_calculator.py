15ab6fb09d6b51fb6c54b944968f4e4f5cd09818
shravan kalyankar <shravankalyankar10@gmail.com>
2025-09-04 17:17:37 +0530

    3: (fail test) testing with single number

--- app/tests/test_string_calculator.py
+++ app/tests/test_string_calculator.py
@@ -3,3 +3,4 @@ from app.src.string_calculator import StringCalculator
 def test_string_calculator():
     calculator = StringCalculator()
     assert calculator.add('') == 0
+    assert calculator.add('1') == 1
